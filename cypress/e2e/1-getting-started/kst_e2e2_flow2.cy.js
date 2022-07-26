d6318ced7331c2d008c5095232ffe033b5e02899
alexanderyudin <udilis38@gamil.com>
2022-07-26 11:00:20 +0300

    include cookie in request

--- cypress/e2e/1-getting-started/kst_e2e2_flow2.cy.js
+++ cypress/e2e/1-getting-started/kst_e2e2_flow2.cy.js
@@ -1,8 +1,10 @@
 describe('supplier flow', () => {
-    it('parse token from HTML', function () {
 
+    let cookie
 
-        cy.request('https://test.keysourcingtool.com/')
+    beforeEach('parse token from HTML', function () {
+
+        cy.request('https://test.keysourcingtool.com/users/sign_in')
             .its('body')
             .then((body) => {
 
@@ -15,67 +17,54 @@ describe('supplier flow', () => {
                     'utf8': "✓",
                     'authenticity_token': csrf,
                     'user[email]': 'udilis38@gmail.com',
-                    'user[password]': 'Asdqwe123!',
+                    'user[password]': 'qwerty',
                     'user[remember_me]': "0",
                     'commit': "Logga+in"
                 })
-                    .its('body')
             })
+            .its('body')
     })
-        it('should open edit users info page', function () {
-            cy.visit('https://test.keysourcingtool.com/')
-            cy.get("input[placeholder=\"user@domain.com\"]")
-                .type('udilis38@gmail.com');
-            cy.get("input[id=\"user_password\"]")
-                .type('Asdqwe123!')
-            cy.get('label[class="checkbox"]')
-                .click()
-
-            cy.getCookie('_cat_session').should('exist')
-        });
+                it('saving cookie', function () {
 
 
-    it('is doing something very important', (done) => {
-        // this event will automatically be unbound when this
-        // test ends because it's attached to 'cy'
+                    cy.getCookie('_cat_session')
+                        .should('exist')
+                        .then((c) => {
 
-        cy.on('uncaught:exception', (err, runnable) => {
-            expect(err.message).to.include('Blocked loading mixed active content "http://test.keysourcingtool.com/" __\n')
+                            // save cookie until need it
 
-            // using mocha's async done callback to finish
-            // this test so we prove that an uncaught exception
-            // was thrown
-            done()
-
-            // return false to prevent the error from
-            // failing this test
-            return false
-        })
-
-        cy.request('https://test.keysourcingtool.com/')
-            .its('body')
-            .then((body) => {
-
-                // using Cypress.$ to parse the string body
-
-                const $html = Cypress.$(body)
-                const csrf = $html.find('input[name="authenticity_token"]').val()
-
-                cy.request('POST', 'https://test.keysourcingtool.com/users/sign_in', {
-                    'utf8': "✓",
-                    'authenticity_token': csrf,
-                    'user[email]': 'udilis38@gmail.com',
-                    'user[password]': 'Asdqwe123!',
-                    'user[remember_me]': "0",
-                    'commit': "Logga+in"
+                            cookie = c
+                        })
                 })
-                    .its('body')
-            })
 
-        // assume this causes an error
+
+    it('fill the form', function () {
+        cy.visit('https://test.keysourcingtool.com/')
+
+        cy.get("input[class=\" string email required \"]")
+            .type('udilis38@gmail.com');
+        cy.get("input[class=\" password optional \"]")
+            .type('qwerty')
+        cy.get('label[class="checkbox"]')
+            .click()
+
+    })
+
+    it('click log in', function () {
+
+        // send correct request
         cy.get('input[value="Logga in"]')
             .click()
+            .then(() => {
+                cy.request({
+                    url: 'https://test.keysourcingtool.com/users/sign_in',
+                    headers: {
+                        'Cookie': cookie.value,
+                    },
+                })
+            })
     })
-
-
 })
+
+
+
